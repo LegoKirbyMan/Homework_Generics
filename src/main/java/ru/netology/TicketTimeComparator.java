a002d4f640bdad1c2a4aafcb841b7b7470200c4f
LegoKirbyMan <timofei.sadownikov@yandex.ru>
2025-01-31 00:46:26 +0300

    Initial commit

---
+++ src/main/java/ru/netology/TicketTimeComparator.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+    
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        
+        return 2;
+    }
+}
